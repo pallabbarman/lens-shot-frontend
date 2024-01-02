82b7a4735de95990e293e6de8e4874065c573db2
Pallab Bharman <bharmanpallab@gmail.com>
2024-01-02 14:23:33 +0600

    not found and error page design updated

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,41 @@
 'use client';
 
+import error from '@/assets/server-error.svg';
+import { cssColor } from '@/utils/color';
+import { Box, Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Error page</h1>
-        </div>
+        <Box
+            height="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+        >
+            <Box width={300} height={350} position="relative">
+                <Image src={error} alt="error" fill />
+            </Box>
+            <Box
+                display="flex"
+                alignItems="center"
+                flexDirection="column"
+                gap={3}
+            >
+                <Typography variant="text3" color="textSecondary">
+                    Sorry, something went wrong. Please try again!
+                </Typography>
+                <Button
+                    LinkComponent={Link}
+                    href="/"
+                    sx={{ color: cssColor('stormgrey') }}
+                >
+                    Back home
+                </Button>
+            </Box>
+        </Box>
     );
 };
 
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,40 @@
+import notFound from '@/assets/not-found.svg';
+import { cssColor } from '@/utils/color';
+import { Button, Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Image from 'next/image';
+import Link from 'next/link';
+
 const NotFoundPage = () => {
     return (
-        <div>
-            <h1>Not found!</h1>
-        </div>
+        <Box
+            height="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+        >
+            <Box width={300} height={350} position="relative">
+                <Image src={notFound} alt="not-found" fill />
+            </Box>
+            <Box
+                display="flex"
+                alignItems="center"
+                flexDirection="column"
+                gap={3}
+            >
+                <Typography variant="text3" color="textSecondary">
+                    Sorry, the page you visited does not exist.
+                </Typography>
+                <Button
+                    LinkComponent={Link}
+                    href="/"
+                    sx={{ color: cssColor('stormgrey') }}
+                >
+                    Back home
+                </Button>
+            </Box>
+        </Box>
     );
 };
 
