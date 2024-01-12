840b8f9963b44a27ad8e41654abe20273e2923a5
Pallab Bharman <bharmanpallab@gmail.com>
2024-01-12 17:38:34 +0600

    blog api integrated

--- src/components/BlogCard/index.tsx
+++ src/components/BlogCard/index.tsx
@@ -1,27 +1,30 @@
+import { IBlog } from '@/types/blog';
 import { Box, Card, CardProps, Tooltip, Typography } from '@mui/material';
-import Image, { StaticImageData } from 'next/image';
+import Image from 'next/image';
 import Link from 'next/link';
-import { ReactNode } from 'react';
 
 interface BlogCardProps extends CardProps {
-    img: string | StaticImageData;
-    alt?: string;
-    children?: ReactNode;
+    blog: IBlog;
 }
 
-const BlogCard = ({ img, alt, title, children, ...props }: BlogCardProps) => {
+const BlogCard = ({ blog, ...props }: BlogCardProps) => {
     return (
         <Card
             component={Link}
-            href="/"
-            sx={{ borderRadius: 0, boxShadow: 'none', textDecoration: 'none' }}
+            href={`/blog/${blog.id}`}
+            sx={{
+                borderRadius: 0,
+                boxShadow: 'none',
+                textDecoration: 'none',
+            }}
             {...props}
         >
             <Image
-                src={img}
-                alt={alt || 'blog'}
+                src={blog.img}
+                alt={blog.title}
                 height={230}
                 width={0}
+                sizes="100vw"
                 style={{
                     width: '100%',
                     objectFit: 'cover',
@@ -29,31 +32,29 @@ const BlogCard = ({ img, alt, title, children, ...props }: BlogCardProps) => {
             />
             <Box my={1}>
                 <Typography variant="text5" color="textSecondary">
-                    Sep 25, 2022 | Wedding
+                    {blog.createdAt} | {blog.category.name}
                 </Typography>
             </Box>
-            <Tooltip placement="top" title={title}>
+            <Tooltip placement="top" title={blog.title}>
                 <Typography variant="h2" mb={0.5} noWrap fontWeight={400}>
-                    {title}
+                    {blog.title}
                 </Typography>
             </Tooltip>
-            {children && (
-                <Typography
-                    variant="text4"
-                    fontWeight={400}
-                    color="textSecondary"
-                    sx={{
-                        WebkitLineClamp: '3',
-                        WebkitBoxOrient: 'vertical',
-                        overflow: 'hidden',
-                        textOverflow: 'ellipsis',
-                        display: '-webkit-box',
-                        wordBreak: 'break-all',
-                    }}
-                >
-                    {children}
-                </Typography>
-            )}
+            <Typography
+                variant="text4"
+                fontWeight={400}
+                color="textSecondary"
+                sx={{
+                    WebkitLineClamp: '3',
+                    WebkitBoxOrient: 'vertical',
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    wordBreak: 'break-all',
+                }}
+            >
+                {blog.content}
+            </Typography>
         </Card>
     );
 };
---
+++ src/types/category.ts
@@ -0,0 +1,6 @@
+export interface ICategory {
+    id?: string;
+    name: string;
+    createdAt?: string;
+    updatedAt?: string;
+}
